4743fa8848a91595b103360d86509505775ba10b
ikebastuz <ikebastuz@gmail.com>
2018-05-06 20:30:58 +0300

    login authorization

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://ikebastuz:bambu44a@ds117070.mlab.com:17070/ike_dev_connector"
+    "mongodb://ikebastuz:bambu44a@ds117070.mlab.com:17070/ike_dev_connector",
+  secretOrKey: "tumba-jumba"
 };
