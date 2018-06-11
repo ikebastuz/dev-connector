4612ab48b0cf23468cbfc1e5a52dec6a6c45ad33
ikebastuz <ikebastuz@gmail.com>
2018-06-11 07:59:10 +0300

    dashboard finished

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -55,7 +55,6 @@ export const clearCurrentProfile = () => {
 };
 
 // Add experience
-
 export const addExperience = (expData, history) => dispatch => {
   axios
     .post("/api/profile/experience", expData)
@@ -69,11 +68,46 @@ export const addExperience = (expData, history) => dispatch => {
 };
 
 // Add education
-
 export const addEducation = (eduData, history) => dispatch => {
   axios
     .post("/api/profile/education", eduData)
     .then(res => history.push("/dashboard"))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Delete Education
+export const deleteEducation = id => dispatch => {
+  axios
+    .delete(`/api/profile/education/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
